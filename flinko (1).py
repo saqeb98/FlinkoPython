9ac75a20bc37b256487acbd6f86362b78e0b9077
saqeb98 <76609145+saqeb98@users.noreply.github.com>
2022-07-01 09:25:26 +0530

    Update flinko (1).py

--- flinko (1).py
+++ flinko (1).py
@@ -23,7 +23,7 @@ def login(mail, password):
     token = tk['responseObject']['access_token']
     head["Authorization"] = "Bearer " + token
 
-    suiteid = 'SUITE1005'
+    suiteid = 'SUITE1017'
     pes = s.post('https://app.flinko.com:8109/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head)
     out = json.loads(pes.content)
     exid = out['responseObject']['id']
@@ -48,4 +48,4 @@ def login(mail, password):
             sc = 1
         time.sleep(10)
 
-login('sanjana.b@testyantra.com', 'Password@123')
+login('mohammed.saqeb@testyantra.com', 'Password@123')
